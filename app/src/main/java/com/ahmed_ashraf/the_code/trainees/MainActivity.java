1c827416635104c0a0e1294e297ecbd928128ced
ahmed-ashraf19 <41925852+ahmed-ashraf19@users.noreply.github.com>
2019-01-11 21:29:17 +0200

    Initial commit

---
+++ app/src/main/java/com/ahmed_ashraf/the_code/trainees/MainActivity.java
@@ -0,0 +1,13 @@
+package com.ahmed_ashraf.the_code.trainees;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
